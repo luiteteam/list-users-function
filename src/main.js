57e683efc51252072c5bf3ad8c58e193c1dceca5
Mahil Sonowal <155747136+mahilsonowal@users.noreply.github.com>
2025-07-09 18:50:31 +0530

    refactor: improve user listing function by logging total users and cleaning up code

--- src/main.js
+++ src/main.js
@@ -1,31 +1,35 @@
 import { Client, Users } from 'node-appwrite';
 
+// This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
+  // You can use the Appwrite SDK to interact with other services
+  // For this example, we're using the Users service
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(process.env.APPWRITE_API_KEY);
-
+    .setKey(req.headers['x-appwrite-key'] ?? '');
   const users = new Users(client);
 
   try {
     const response = await users.list();
-    // Filter out admins
-    const filtered = response.users.filter(
-      (u) => !u.prefs || u.prefs.role !== "admin"
-    );
-    // Only return the fields needed for the dashboard
-    const safeUsers = filtered.map((u) => ({
-      $id: u.$id,
-      name: u.name,
-      email: u.email,
-      emailVerification: u.emailVerification,
-      registration: u.registration,
-    }));
-    log(JSON.stringify(safeUsers));
-    return res.json({ users: safeUsers });
-  } catch (err) {
+    // Log messages and errors to the Appwrite Console
+    // These logs won't be seen by your end users
+    log(`Total users: ${response.total}`);
+  } catch(err) {
     error("Could not list users: " + err.message);
-    return res.json({ error: err.message }, 500);
   }
+
+  // The req object contains the request data
+  if (req.path === "/ping") {
+    // Use res object to respond with text(), json(), or binary()
+    // Don't forget to return a response!
+    return res.text("Pong");
+  }
+
+  return res.json({
+    motto: "Build like a team of hundreds_",
+    learn: "https://appwrite.io/docs",
+    connect: "https://appwrite.io/discord",
+    getInspired: "https://builtwith.appwrite.io",
+  });
 };
